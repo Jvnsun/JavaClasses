c694384bc2e2975032a32ca643e93352a7119dd8
Evheniy <qtug@yandex.com>
2018-06-30 21:47:27 +0300

    Update Main.java

--- Block01/src/com/Jvnsun/Main.java
+++ Block01/src/com/Jvnsun/Main.java
@@ -6,7 +6,7 @@ import com.Jvnsun.View.View;
 public class Main {
 
     public static void main(String[] args) {
-	    Model model = new Model();
+	Model model = new Model();
         View view = new ConsoleView();
         Controller controller = new Controller(model, view);
         controller.processUser();
