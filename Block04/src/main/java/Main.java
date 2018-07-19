b68f0e1d984571acb0721891b33a5bf568ea1b02
Evgeniy <qtug@yandex.com>
2018-07-19 16:43:23 +0300

    4 final

--- Block04/src/main/java/Main.java
+++ Block04/src/main/java/Main.java
@@ -1,12 +1,12 @@
 import controller.Controller;
 import model.api.Notebook;
-import model.impl.PersonForm;
 import model.impl.ListNotebook;
+import model.impl.PersonForm;
 import view.ConsoleView;
 
 public class Main {
   public static void main(String[] args) {
-    Notebook notebook = new ListNotebook(PersonForm.Surname, PersonForm.Phone);
+    Notebook notebook = new ListNotebook(PersonForm.Name, PersonForm.Surname, PersonForm.Nick, PersonForm.Phone);
     ConsoleView view = new ConsoleView();
     Controller controller = new Controller(notebook, view);
     controller.createNote();
